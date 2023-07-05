71161bb7ea826283bd49340f65b001e72dd6d6d9
Laureano <laumaidana56@gmail.com>
2023-07-04 21:00:11 -0300

    El js permite que las luces se prendan random

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-var botones = document.querySelectorAll(
-  ".verde",
-  ".rojo",
-  ".azul",
-  ".amarillo"
-);
+var botones = document.querySelectorAll(".juego button");
+var tiempoEncendido = 2000; // Duración del encendido en milisegundos
+var tiempoApagado = 500; // Tiempo entre encendidos en milisegundos
 
-function encenderBoton() {
-  var botonAleatorio = botones[Math.floor(Math.random() * botones.length)];
-  botonAleatorio.classList.add("active");
+function encenderBotonElegido() {
+  // Apagar todos los botones
+  for (var i = 0; i < botones.length; i++) {
+    botones[i].classList.remove("active");
+  }
 
+  // Obtener un botón aleatorio
+  var botonAleatorio = Math.floor(Math.random() * botones.length);
+  var botonElegido = botones[botonAleatorio];
+
+  // Encender el botón aleatorio
+  botonElegido.classList.add("active");
+
+  // Esperar tiempoEncendido milisegundos y apagar el botón
   setTimeout(function () {
-    botonAleatorio.classList.remove("active");
-  }, 500);
-}
-function iniciarSecuencia() {
-  setInterval(encenderBoton, 2000);
+    botonElegido.classList.remove("active");
+  }, tiempoEncendido);
 }
 
-encenderBoton();
-iniciarSecuencia();
+// Llamar a la función inicialmente
+encenderBotonElegido();
+
+// Encender un botón aleatorio cada 3 segundos
+setInterval(encenderBotonElegido, tiempoApagado + tiempoEncendido);
