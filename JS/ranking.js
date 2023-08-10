8a15413067415f2c204e7a6fba06eaf2781943d4
Laureano <laumaidana56@gmail.com>
2023-08-10 00:20:08 -0300

    Se puso una letra mas compatible

--- JS/ranking.js
+++ JS/ranking.js
@@ -3,15 +3,13 @@ var listaPartidas = document.querySelector(".listaPartidas");
 var contenido = document.getElementById("contenido");
 var obtenerPartidas = localStorage.getItem("partidasJugadas");
 var partidasJugadas = JSON.parse(obtenerPartidas);
+var ordenarPuntaje = document.getElementById("ordenarPorPuntaje");
+var ordenarFecha = document.getElementById("ordenarPorFecha");
 
-if (partidasJugadas) {
-  partidasJugadas.sort(function (a, b) {
-    return b.puntajeTotal - a.puntajeTotal;
-  });
-
+// Función para mostrar partidas en la tabla
+function mostrarPartidas(partidas) {
   contenido.innerHTML = "";
-
-  partidasJugadas.forEach(function (dato) {
+  partidas.forEach(function (dato) {
     var fila = document.createElement("tr");
     fila.innerHTML =
       "<td>" +
@@ -30,6 +28,25 @@ if (partidasJugadas) {
       dato.hora +
       "</td>";
     contenido.appendChild(fila);
+  });
+}
+
+if (partidasJugadas) {
+  mostrarPartidas(partidasJugadas);
+  ordenarPuntaje.addEventListener("click", function () {
+    partidasJugadas.sort(function (a, b) {
+      return b.puntajeTotal - a.puntajeTotal;
+    });
+    mostrarPartidas(partidasJugadas);
+  });
+
+  ordenarFecha.addEventListener("click", function () {
+    partidasJugadas.sort(function (a, b) {
+      var fechaA = new Date(a.fecha + " " + a.hora);
+      var fechaB = new Date(b.fecha + " " + b.hora);
+      return fechaB - fechaA;
+    });
+    mostrarPartidas(partidasJugadas);
   });
 } else {
   console.log("No hay partidas jugadas o el valor no es válido.");
